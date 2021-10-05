8615e31802debc5782af48c162adef798c858c6f
pavolhodas <pavolhodas4@gmail.com>
2021-10-05 10:23:52 +0200

    student class creation

--- src/JavaDeeper.java
+++
@@ -1,5 +0,0 @@
-public class JavaDeeper {
-    public static void main(String[]args){
-        System.out.println("JavaDeeper");
-    }
-}
---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public static String name = "John";
+    public static int studentNumber = 12345;
+
+    public static void main(String[] args) {
+        Student a = new Student();
+
+        System.out.println(a.name + a.studentNumber);
+    }
+}
