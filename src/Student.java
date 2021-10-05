5fa5bf379446d9294ccd5c6c51a347215bfb998a
pavolhodas <pavolhodas4@gmail.com>
2021-10-05 10:31:20 +0200

    edit to more correct code

--- src/Student.java
+++ src/Student.java
@@ -2,9 +2,16 @@ public class Student {
     public static String name = "John";
     public static int studentNumber = 12345;
 
-    public static void main(String[] args) {
-        Student a = new Student();
+    public String getName(){
+        return name;
+    }
+    public int getStudentNumber(){
+        return studentNumber;
+    }
 
-        System.out.println(a.name + a.studentNumber);
+    public static void main(String[] args) {
+        Student student1 = new Student();
+
+        System.out.println(student1.getName() + ": "+ student1.getStudentNumber());
     }
 }
