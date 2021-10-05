7c132979d5a17cceff33492a7f27a7c2b3f69a25
pavolhodas <pavolhodas4@gmail.com>
2021-10-05 15:18:16 +0200

    2. Get Sum of People's Budget

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.StudentNumber = 4;
-        student1.Name = "John";
-        student1.StudentPhoneNumber = +421908625;
-        student1.StudentAdress = "01313 Rajecke teplice";
-
-        System.out.println(student1.getName() + ", "+ student1.getStudentNumber()+", "+student1.getPhoneNumber() +", "+ student1.getStudentAdress());
-
-        Student student2 = new Student();
-        student2.StudentNumber = 9;
-        student2.Name = "Sam";
-        student2.StudentPhoneNumber = +421111225;
-        student2.StudentAdress ="01001 Zilina";
-
-        System.out.println(student2.getName() + ", "+ student2.getStudentNumber()+", "+student2.getPhoneNumber() +", "+ student2.getStudentAdress());
+        Student.getStudent();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,23 +1,32 @@
 public class Student {
-    public static String Name;
-    public static String StudentAdress;
-    public static int StudentNumber;
-    public static int StudentPhoneNumber;
+    String Name;
+    int Years;
+    int Budget;
 
-    public String getName(){
-        return Name;
+
+
+    public Student(String name, int years, int budget){
+        Name= name;
+        Years=years;
+        Budget=budget;
+    }
+    public static void getStudent(){
+        int[] budgetArray = new int[3];
+
+        Student student1 = new Student("Palo", 17, 23000 );
+        Student student2 = new Student("Steve", 32, 40000 );
+        Student student3 = new Student("Martin", 16, 2700 );
+
+        budgetArray[0]=student1.Budget;
+        budgetArray[1]=student2.Budget;
+        budgetArray[2]=student3.Budget;
+
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+        System.out.println(budgetArray[0]+budgetArray[1]+budgetArray[2]);
     }
 
-    public String getStudentAdress(){
-        return StudentAdress;
-    }
-    public int getStudentNumber(){
-        return StudentNumber;
-    }
-
-    public int getPhoneNumber(){
-    return StudentPhoneNumber;
-    }
     public static void main(String[] args) {
 
     }
