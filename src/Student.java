9181122c6a3c3063d5b79634daa64ad2918826e7
pavolhodas <pavolhodas4@gmail.com>
2021-10-12 16:59:49 +0200

    Fix the 1b example with students additional details

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,24 @@
 public class Student {
     private String name;
     private int studentNumber;
+    private int studentPhoneNumber;
+    private String studentAddress;
+
+    public int getStudentPhoneNumber() {
+        return studentPhoneNumber;
+    }
+
+    public void setStudentPhoneNumber(int studentPhoneNumber) {
+        this.studentPhoneNumber = studentPhoneNumber;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -20,8 +38,19 @@ public class Student {
 
     public static void main(String[] args) {
         Student student01 = new Student();
-        student01.setName("Jozo");
+        Student student02 = new Student();
+
+        student01.setName("Sam");
         student01.setStudentNumber(1234);
-        System.out.println(student01.getName()+": "+ student01.getStudentNumber());
+        student01.setStudentPhoneNumber(+421589413);
+        student01.setStudentAddress("Legionarska 1, 01001 Zilina");
+
+        student02.setName("John");
+        student02.setStudentNumber(55);
+        student02.setStudentPhoneNumber(456789);
+        student02.setStudentAddress("Legionarska 2, 01001 Zilina");
+
+        System.out.println(student01.getName()+": "+ student01.getStudentNumber()+", " + student01.getStudentPhoneNumber() + ", " + student01.getStudentAddress());
+        System.out.println(student02.getName()+": "+ student02.getStudentNumber()+", " + student02.getStudentPhoneNumber() + ", " + student02.getStudentAddress());
     }
 }
