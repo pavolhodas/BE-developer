4790dd9b226fbb104d1fb36f5ab3074a8bb4b445
pavolhodas <pavolhodas4@gmail.com>
2021-10-16 20:51:53 +0200

    Shiritori with error valid characters

--- src/Shiritori.java
+++ src/Shiritori.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Shiritori {
-    public ArrayList<Words> words;
+    public ArrayList<String> words;
     private static boolean gameOver;
 
     public Shiritori() {
-        this.words = new ArrayList<Words>();
+        this.words = new ArrayList<String>();
     }
 
     @Override
@@ -17,9 +17,9 @@ public class Shiritori {
                 '}';
     }
 
-    private static boolean runGame(Words words01, Words words02, Words words03, Words words04){
-        Shiritori shiritori = new Shiritori();
+    private static boolean runGame(String words01,String words02, String words03, String words04){
         Shiritori correctWords = new Shiritori();
+        Shiritori shiritori = new Shiritori();
 
         shiritori.words.add(words01);
         shiritori.words.add(words02);
@@ -35,15 +35,21 @@ public class Shiritori {
         return false;
     }
 
-    private static boolean play(ArrayList<Words> words,Shiritori correctWords) {
+    private static boolean play(ArrayList<String> words,Shiritori correctWords) {
 
         if (gameOver) {
-            System.out.println("game over");
+            restart();
             gameOver = false;
             return false;
         }
+        final String validChars = "0123456789+-*/()[]{}";
         for (int i = 1; i < words.size(); i++) {
-            if (words.get(i-1).Name.charAt(words.get(i-1).Name.length() - 1) == words.get(i).Name.charAt(0) && words.get(i-1) != words.get(i)) {
+            for(int a = 0; a < words.get(i-1).length()-1; a++) {
+                if(!validChars.contains(words.get(i-1).charAt(a))) {
+                    System.out.println("invalid string, try again: ");
+                }
+            }
+            if (words.get(i-1).charAt(words.get(i-1).length() - 1) == words.get(i).charAt(0) && words.get(i-1) != words.get(i)) {
                 correctWords.words.add(words.get(i-1));
             }
 
@@ -55,23 +61,34 @@ public class Shiritori {
         correctWords.words.add(words.get(words.size()-1));
         return true;
     }
-    public static boolean restart(){
-        Words words01 = new Words("");
-        Words words02 = new Words("");
-        Words words03 = new Words("");
-        Words words04 = new Words("");
+
+    private static boolean restart()
+    {
+        runGame("", "", "", "");
         System.out.println("words were deleted");
 
         return false;
     }
 
-        public static void main (String[]args){
-        Words words01 = new Words("Palo");
-        Words words02 = new Words("ovca");
-        Words words03 = new Words("anglicky");
-        Words words04 = new Words("ypsilon");
+    private static String getWords(){
+        Shiritori shiritori = new Shiritori();
 
-            runGame(words01, words02,words03,words04);
+        String words01 = new String("halo");
+        String words02 = new String("ozaj");
+        String words03 = new String("jez");
+        String words04 = new String("oskvarky");
+
+        shiritori.words.add(words01);
+        shiritori.words.add(words02);
+        shiritori.words.add(words03);
+        shiritori.words.add(words04);
+
+        return String.valueOf(shiritori);
+    }
+        public static void main (String[]args){
+            runGame("Palo", "ovca", "anglicky", "ypsilon");
+
+            System.out.println(getWords());
         }
 
 }
