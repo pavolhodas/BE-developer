fd4cbf1bec53beec2be6b45eefcee059786d83f0
pavolhodas <pavolhodas4@gmail.com>
2021-10-19 18:38:10 +0200

    Triangle method clear code

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,7 +1,7 @@
 public class Triangle {
-    private static int sideA;
-    private static int sideB;
-    private static int sideC;
+    private int sideA;
+    private int sideB;
+    private int sideC;
 
     public Triangle(int sideA, int sideB, int sideC) {
         this.sideA = sideA;
@@ -9,16 +9,16 @@ public class Triangle {
         this.sideC = sideC;
     }
 
-    public static int getArea(Triangle triangle){
+    public int getArea(Triangle triangle){
         return sideA*sideB/2;
     }
 
-    public static int getPerimeter(Triangle triangle){
+    public int getPerimeter(Triangle triangle){
         return sideA+sideB+sideC;
     }
     public static void main(String[] args) {
         Triangle triangle01 = new Triangle(6,4,4);
-        System.out.println(getArea(triangle01));
-        System.out.println(getPerimeter(triangle01));
+        System.out.println(triangle01.getArea(triangle01));
+        System.out.println(triangle01.getPerimeter(triangle01));
     }
 }
