44dbad9bb71d958cd2e864200eec7bf8bf6bdcb0
pavolhodas <pavolhodas4@gmail.com>
2021-10-21 19:45:25 +0200

    Clear code of War of warriors

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -7,10 +8,19 @@ public class Main {
         Item items02 = new Item("gold fish", 100);
         Item items03 = new Item("watches", 30);
 
-        //create two warriors
-        Warrior warrior1 = new Warrior("Warrior1", 9, 5, 7, Arrays.asList(items01, items03, items02));
-        Warrior warrior2 = new Warrior("Warrior2", 9, 9, 8, Arrays.asList(items01, items03, items02));
+        //create list of items
+        ArrayList<Item> warrior1Items = new ArrayList<Item>(){};
+        warrior1Items.add(items03);
 
-        //Warrior.warrior01.warOfWariors(warrior1, warrior2);
+        ArrayList<Item> warrior2Items = new ArrayList<Item>(){};
+        warrior2Items.add(items02);
+        warrior2Items.add(items01);
+
+        //create two warriors
+        Warrior warrior01 = new Warrior("Warrior1", 9, 5, 7, warrior1Items);
+        Warrior warrior02 = new Warrior("Warrior2", 9, 9, 8, warrior2Items);
+
+        Warrior.warOfWariors(warrior01, warrior1Items, warrior02, warrior2Items);
+
     }
 }
--- src/Warrior.java
+++ src/Warrior.java
@@ -7,9 +7,9 @@ public class Warrior {
     private int life;
     private int speed;
     private int muscle;
-    private List<Item> items;
+    private ArrayList<Item> items;
 
-    public Warrior(String name, int life, int speed, int muscle, List<Item> items) {
+    public Warrior(String name, int life, int speed, int muscle, ArrayList<Item> items) {
         this.name = name;
         this.life = life;
         this.speed = speed;
@@ -33,58 +33,54 @@ public class Warrior {
         return life + speed + muscle;
     }
 
-    private Item removeHighestItem() {
+    private Item getHighestItem() {
         Item itemForCompare = items.get(0);
         for (int i = 0; i < items.size(); i++) {
             if (items.get(i).value > itemForCompare.value) {
                 itemForCompare = items.get(i);
             }
         }
-        items.remove(itemForCompare);
+        //items.remove(itemForCompare);
         return itemForCompare;
     }
 
-    public static void warOfWariors(Warrior warrior01, Warrior warrior02) {
+    public static void warOfWariors(Warrior warrior01 ,ArrayList<Item> warrior1Items, Warrior warrior02,ArrayList<Item> warrior2Items) {
 
-        //create a list which help to move items
-        ArrayList<Item> removedItem = new ArrayList<Item>();
-
-        if (warrior01.getTotalForce() == 0 && warrior02.getTotalForce() == 0) {
-
-        }
         //if warrior02 is the winner
         if (warrior01.getTotalForce() < warrior02.getTotalForce()) {
 
             if (warrior01.items.size() == 1) {
-                warrior01.items.add(warrior01.items.get(0));
+                warrior2Items.add(warrior01.items.get(0));
+                System.out.println(warrior01.items.get(0)+"from warrior01 was moved to warrior02");
+                warrior1Items.remove(warrior01.items.get(0));
+
             } else if (warrior01.items.size() == 0) {
                 System.out.println("No one item to move");
             } else {
-                warrior02.items.add(warrior01.removeHighestItem());
+                warrior2Items.add(warrior01.getHighestItem());
+                System.out.println(warrior01.getHighestItem()+"from warrior01 was moved to warrior02");
+                warrior1Items.remove(warrior01.getHighestItem());
             }
-
+        //if warrior01 is the winner
         } else if (warrior01.getTotalForce() > warrior02.getTotalForce()) {
+
+            if (warrior02.items.size() == 1) {
+                warrior1Items.add(warrior02.items.get(0));
+                System.out.println(warrior02.items.get(0)+"from warrior02 was moved to warrior01");
+                warrior2Items.remove(warrior02.items.get(0));
+            }
             if (warrior02.items.size() == 0) {
                 System.out.println("No one item to move");
-            } else {
-                warrior01.items.add(warrior02.removeHighestItem());
+            } else{
+                warrior1Items.add(warrior02.getHighestItem());
+                System.out.println(warrior02.getHighestItem()+"from warrior02 was moved to warrior01");
+                warrior2Items.remove(warrior02.getHighestItem());
             }
-
+        //if it is draw
         } else if (warrior01.getTotalForce() == warrior02.getTotalForce()) {
-
+            System.out.println("No one item was moved");
         }
-    }
-
-    public static void main(String[] args) {
-        //create items
-        Item items01 = new Item("compass", 54);
-        Item items02 = new Item("gold fish", 100);
-        Item items03 = new Item("watches", 30);
-
-        //create two warriors
-        Warrior warrior1 = new Warrior("Warrior1", 9, 5, 7, Arrays.asList(items01, items03, items02));
-        Warrior warrior2 = new Warrior("Warrior2", 9, 9, 8, Arrays.asList(items01, items03, items02));
-
-        warOfWariors(warrior1, warrior2);
+        System.out.println(warrior01);
+        System.out.println(warrior02);
     }
 }
