08b0947a38a5b2fce75384f7430a58ecd3ef24d2
pavolhodas <pavolhodas4@gmail.com>
2021-10-22 19:01:28 +0200

    Last clear code of War of warriors

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //create items
-        Item items01 = new Item("compass", 54);
-        Item items02 = new Item("gold fish", 100);
-        Item items03 = new Item("watches", 30);
 
         //create list of items
         ArrayList<Item> warrior1Items = new ArrayList<Item>(){};
-        warrior1Items.add(items03);
+        warrior1Items.add(new Item("watches", 30));
 
         ArrayList<Item> warrior2Items = new ArrayList<Item>(){};
-        warrior2Items.add(items02);
-        warrior2Items.add(items01);
+        warrior2Items.add(new Item("compass", 54));
+        warrior2Items.add(new Item("gold fish", 100));
 
         //create two warriors
         Warrior warrior01 = new Warrior("Warrior1", 9, 5, 7, warrior1Items);
         Warrior warrior02 = new Warrior("Warrior2", 9, 9, 8, warrior2Items);
 
-        Warrior.warOfWariors(warrior01, warrior1Items, warrior02, warrior2Items);
+        Warrior.warOfWariors(warrior01, warrior02);
 
     }
 }
--- src/Warrior.java
+++ src/Warrior.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Warrior {
     private String name;
@@ -44,37 +42,37 @@ public class Warrior {
         return itemForCompare;
     }
 
-    public static void warOfWariors(Warrior warrior01 ,ArrayList<Item> warrior1Items, Warrior warrior02,ArrayList<Item> warrior2Items) {
+    public static void warOfWariors(Warrior warrior01 , Warrior warrior02) {
 
         //if warrior02 is the winner
         if (warrior01.getTotalForce() < warrior02.getTotalForce()) {
 
             if (warrior01.items.size() == 1) {
-                warrior2Items.add(warrior01.items.get(0));
+                warrior02.items.add(warrior01.items.get(0));
                 System.out.println(warrior01.items.get(0)+"from warrior01 was moved to warrior02");
-                warrior1Items.remove(warrior01.items.get(0));
+                warrior01.items.remove(warrior01.items.get(0));
 
             } else if (warrior01.items.size() == 0) {
                 System.out.println("No one item to move");
             } else {
-                warrior2Items.add(warrior01.getHighestItem());
+                warrior02.items.add(warrior01.getHighestItem());
                 System.out.println(warrior01.getHighestItem()+"from warrior01 was moved to warrior02");
-                warrior1Items.remove(warrior01.getHighestItem());
+                warrior01.items.remove(warrior01.getHighestItem());
             }
         //if warrior01 is the winner
         } else if (warrior01.getTotalForce() > warrior02.getTotalForce()) {
 
             if (warrior02.items.size() == 1) {
-                warrior1Items.add(warrior02.items.get(0));
+                warrior01.items.add(warrior02.items.get(0));
                 System.out.println(warrior02.items.get(0)+"from warrior02 was moved to warrior01");
-                warrior2Items.remove(warrior02.items.get(0));
+                warrior02.items.remove(warrior02.items.get(0));
             }
             if (warrior02.items.size() == 0) {
                 System.out.println("No one item to move");
             } else{
-                warrior1Items.add(warrior02.getHighestItem());
+                warrior01.items.add(warrior02.getHighestItem());
                 System.out.println(warrior02.getHighestItem()+"from warrior02 was moved to warrior01");
-                warrior2Items.remove(warrior02.getHighestItem());
+                warrior02.items.remove(warrior02.getHighestItem());
             }
         //if it is draw
         } else if (warrior01.getTotalForce() == warrior02.getTotalForce()) {
@@ -82,5 +80,22 @@ public class Warrior {
         }
         System.out.println(warrior01);
         System.out.println(warrior02);
+    }
+    public static void main(String[] args) {
+
+        //create list of items
+        ArrayList<Item> warrior1Items = new ArrayList<Item>(){};
+        warrior1Items.add(new Item("watches", 30));
+
+        ArrayList<Item> warrior2Items = new ArrayList<Item>(){};
+        warrior2Items.add(new Item("compass", 54));
+        warrior2Items.add(new Item("gold fish", 100));
+
+        //create two warriors
+        Warrior warrior01 = new Warrior("Warrior1", 9, 5, 7, warrior1Items);
+        Warrior warrior02 = new Warrior("Warrior2", 9, 9, 8, warrior2Items);
+
+        Warrior.warOfWariors(warrior01, warrior02);
+
     }
 }
