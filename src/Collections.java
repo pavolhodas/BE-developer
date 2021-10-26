45a654c06bb90f691876350345ef117b32017aa2
pavolhodas <pavolhodas4@gmail.com>
2021-10-26 08:28:22 +0200

    change code to right lambada and reference method

--- src/Collections.java
+++ src/Collections.java
@@ -1,41 +1,31 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiFunction;
 
 public class Collections {
 
-    List<Integer> referenceNumsInList = new ArrayList<>();
-    public static int add(int a, int b) {
-        Collections collections = new Collections();
+    public static void getIntegersLambada(List<Integer> integers) {
 
-        collections.referenceNumsInList.add(a);
-        collections.referenceNumsInList.add(b);
-        System.out.println(collections.referenceNumsInList);
 
-        return a+b;
+        integers.forEach(i -> System.out.println(i));
+        if (integers.isEmpty()) System.out.println("List je prazdny");
+
+        else System.out.println(integers);
     }
 
-    public static void getIntegers(List<Integer> integers) {
-        List<Integer> numOfList = new ArrayList<>();
+    public static void getIntegersReference(List<Integer> integers) {
 
-        integers.forEach((num) -> {
-            numOfList.add(num);
-        });
-        if (numOfList.isEmpty()) System.out.println("List je prazdny");
+        integers.forEach(System.out::println);
+        if (integers.isEmpty()) System.out.println("List je prazdny");
 
-        else System.out.println(numOfList);
+        else System.out.println(integers);
     }
 
     public static void main(String[] args) {
 
-        BiFunction<Integer, Integer, Integer> adder1 = Collections::add;
-        System.out.println("Method Reference:");
-        int result1 = adder1.apply(10, 20);
-        int result2 = adder1.apply(10,5);
-        int result3 = adder1.apply(4,8);
-
         System.out.println("Lambda function:");
-        getIntegers(Arrays.asList(result1, result2, result3));
+        getIntegersLambada(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5 ));
+
+        System.out.println("Method reference:");
+        getIntegersReference(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5 ));
     }
 }
