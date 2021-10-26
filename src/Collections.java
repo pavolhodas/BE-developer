05cc72282da7b436d27a6ac78d7bfe07d5d012f8
pavolhodas <pavolhodas4@gmail.com>
2021-10-26 08:46:43 +0200

    2.exercise create method addIfNotExists

--- src/Collections.java
+++ src/Collections.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,6 +21,17 @@ public class Collections {
         else System.out.println(integers);
     }
 
+    private static void addIfNotExists(List<Integer> integers, int newItem){
+        List<Integer> listOfNums = new ArrayList<>();
+
+        integers.forEach(listOfNums::add);
+        if (integers.contains(newItem)) System.out.println("new item already exist in this list");
+
+        else {
+            listOfNums.add(newItem);
+            System.out.println(listOfNums);
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("Lambda function:");
@@ -27,5 +39,8 @@ public class Collections {
 
         System.out.println("Method reference:");
         getIntegersReference(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5 ));
+
+        System.out.println("Add integer to list:");
+        addIfNotExists(Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5 ), 9);
     }
 }
